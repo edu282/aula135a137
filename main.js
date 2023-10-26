1e64e94cfdeb9605c00cd166008322bd85ebe41f
edu282 <91227097+edu282@users.noreply.github.com>
2023-10-26 16:48:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+video = "";
+modelStatus = "";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded()
+{
+    console.log("Modelo Carregado!");
+    modelStatus = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
